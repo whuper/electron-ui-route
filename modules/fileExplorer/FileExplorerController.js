f06012894116071fb73f505b9f1b9818957866ab
wangwh <wangwenh@ngarihealth.com>
2017-08-15 18:27:07 +0800

    0815

--- modules/fileExplorer/FileExplorerController.js
+++ modules/fileExplorer/FileExplorerController.js
@@ -8,45 +8,56 @@
 	
 	var app = require('electron').remote.app;
 	var appCfg = app.sysConfig();
-	var os = appCfg.platform;
-	console.log('os',os);
-	if(os == 'darwin'){
+	var system = appCfg.platform;
+	console.log('system',system);
+	/*if(system == 'darwin'){
 		var curDirPath = 'D:/nodejs/electron/electron-ui-route/';
 	}
-	if(os == 'win32'){
+	if(system == 'win32'){
 		var curDirPath = 'D:/nodejs/electron/electron-ui-route/';
-	}
+	}*/
 
+	try {
+			//获取自定义皮肤的配置
+	var fs = require('fs');
+	var jsonObj = JSON.parse(fs.readFileSync('./data/config.json',"utf8"));
+
+	}catch(error){
+            console.log(error);
+    }
+
+	var os = require('os');
+	var curDirPath = os.homedir() + '/';
+	curDirPath = curDirPath.replace(/\\/g,'/');
     this.initialize = function () {
 
-
 		$scope.curItemIndex = undefined;
-      $scope.employee = 'obama';  
-      $scope.imagePath = './assets/washedout.png';
-	  var imagePath = 'assets/60.jpeg';
+		$scope.employee = 'obama';  
+		$scope.imagePath = './assets/washedout.png';
+		$scope.disklist = jsonObj.disklist;
+		var imagePath = 'assets/60.jpeg';
 
-	  console.log('###FileExplorerController');
-
-	  
-	
-	/*var rd = require('rd');
-	// 异步列出目录下的所有文件 __dirname
-	rd.read('D:/nodejs/electron/electron-ui-route/modules', function (err, files) {
-	  if (err) throw err;
-	  // files是一个数组，里面是目录/tmp目录下的所有文件（包括子目录）
-	  console.log(files);
-	});*/
-	  $scope.ctrl.changeDir();
+		/*var rd = require('rd');
+		// 异步列出目录下的所有文件 __dirname
+		rd.read('D:/nodejs/electron/electron-ui-route/modules', function (err, files) {
+		if (err) throw err;
+		// files是一个数组，里面是目录/tmp目录下的所有文件（包括子目录）
+		console.log(files);
+		});*/
+		$scope.ctrl.changeDir();
 
 
 	//init end
 	};
 	this.changeDir = function(path){
-
+		if(!path){
+			path = curDirPath;
+		}
 		$scope.ctrl.getCurDirFile(path).then(function(json){
 		$scope.fileList = json;
 		 var pathArray = curDirPath.split("/"); 
 	
+
 		  for(var i= pathArray.length-1; i>0;i--){
              if(pathArray[i] == "")
              {
@@ -69,7 +80,6 @@
 		/*if(path.length < 4){
 		//path = path.substring(0,path.length-1);
 		}*/
-		console.log('path',path);
 		$scope.ctrl.changeDir(path);
 	};
 	this.itemClick = function(item){
@@ -89,39 +99,50 @@
 		}
 
 	}
-	this.sysOpen = function(path){
+	this.sysOpen = function(fpath){
 		var {remote} = require('electron');
 		var {shell} = remote;
-		var os = require('os')
-		//shell.showItemInFolder(path);
-		shell.openItem(path);
+		var os = require('os');
+
+		//fpath = fpath.substring(0,fpath.length-1);
+		fpath = fpath.replace(/\/$/,'');
+		fpath = fpath.replace(/\//g, '\\')
+		console.log(fpath);
+			shell.showItemInFolder(fpath);
 	
+		//shell.openItem(path);
 	
 	};
 	this.getCurDirFile = function(fullPath){
-		if(fullPath){
-			curDirPath = fullPath;
-		}	
+	
 		var deferred = $q.defer();
 
-		fs.readdir(curDirPath, function(error, files) {
-			console.log('files',files);
-			if (error) {
+		fs.readdir(fullPath, function(error, files) {
+			if (error || !files) {
+				   $mdDialog.show(
+					  $mdDialog.alert()
+						.title('Operation not permitted!')
+						.textContent(error.path)
+						.ok('OK')
+					);
 			  deferred.reject(error);			 
+			} else {
+					var filelist = [];
+					for (var i = 0; i < files.length; i++) {
+						if(files[i].indexOf('Sys') == -1){
+							var fileinfo = fs.statSync(fullPath + files[i]);
+							
+							if(fileinfo.isDirectory()){
+								filelist.push([files[i],'folder',fileinfo])
+							} else {
+								filelist.push([files[i],'blank',fileinfo])
+							}
+						}
+					};
+					curDirPath = fullPath;
+					deferred.resolve(filelist);			
 			}
-			var filelist = [];
-			for (var i = 0; i < files.length; i++) {
-				if(files[i].indexOf('Sys') == -1){
-					var fileinfo = fs.statSync(curDirPath + files[i]);
-					
-					if(fileinfo.isDirectory()){
-						filelist.push([files[i],'folder',fileinfo])
-					} else {
-						filelist.push([files[i],'blank',fileinfo])
-					}
-				}
-			};
-			deferred.resolve(filelist);
+
 		  });
 
 		return deferred.promise;
@@ -133,7 +154,11 @@
 		var dbFile = "./movies.db";
 		fs.exists(dbFile, function(exists) {  
 			if(exists){
-				alert('数据库已经存在');
+				 $mdDialog.show(
+					  $mdDialog.alert()
+						.textContent('数据库已经存在了')
+						.ok('OK')
+					);
 			} else {
 					var sqlite3 = require("sqlite3").verbose();
 					var db = new sqlite3.Database(dbFile, function(e){
@@ -169,54 +194,68 @@
 	};
 	this.scan = function(){
 
-		var rd = require('rd');
-		var sql3 = require("sqlite3").verbose();
-		var db = new sql3.Database('./movies.db');
+	 var confirm = $mdDialog.confirm()
+          .title('Would you like to scan the hard disk?')
+          .textContent('wawa.wa')
+          .ok('ok')
+          .cancel('No');
 
-		//var filePattern = /^\w*\\.[mp4|rmvb|flv|mpeg|avi|mkv|rm]$/i;
-		var filePattern = /\.(mp4|rmvb|flv|mpeg|avi|mkv|rm)$/i;
-		//cacls "D:System Volume Information" /e /g everyone:f && rd /s /q "D:System Volume Information"
+		$mdDialog.show(confirm).then(function() {
+			var rd = require('rd');
+			var sql3 = require("sqlite3").verbose();
+			var db = new sql3.Database('./movies.db');
 
-		//var timestamp = new Date().getTime();
-		var startTimestamp = Date.parse(new Date())/1000;
+			//var filePattern = /^\w*\\.[mp4|rmvb|flv|mpeg|avi|mkv|rm]$/i;
+			var filePattern = /\.(mp4|rmvb|flv|mpeg|avi|mkv|rm|wmv)$/i;
+			//cacls "D:System Volume Information" /e /g everyone:f && rd /s /q "D:System Volume Information"
 
-		//sql = 'update english set phonetic = ?,wordGroup = ?,example = ? where id = ?'
-		//sql = "insert into lesson_info values ('%s', '%s','%s','%s','%s','%s')" % (name, link, des, number, time, degree)
-		//// 将值封装为一个数组传值.
-		//db.run("UPDATE tbl SET name = ? WHERE id = ?", [ "bar", 2 ]);
-
-		// 异步遍历目录下的所有文件
-		rd.eachFileFilter('e:/',filePattern,function (f, s, next) {
-		  // 每找到一个文件都会调用一次此函数
-		  // 参数s是通过 fs.stat() 获取到的文件属性值
-	
-		  var filefullPath = f.replace(/\\/g,'/');
-		  var arr = filefullPath.split('/');
-		
-			var filename = arr[arr.length - 1];
-
-			var path = filefullPath.replace(filename,'');
-			var parentsfolder = arr[arr.length - 2];
-			var ctime = Date.parse(s.ctime)/1000
-			var mtime = Date.parse(s.mtime)/1000
-			var filesize = s.size;
-			var filetype = filename.substring(filename.lastIndexOf(".")+1);
-
+			//var timestamp = new Date().getTime();
+			var startTimestamp = Date.parse(new Date())/1000;
 			var stmt = db.prepare("INSERT  INTO porns (id,name,mtime,ctime,filesize,filetype,path,parentsfolder) VALUES (?,?,?,?,?,?,?,?)");
-			console.log('filename',filename);
-			stmt.run(null,filename,mtime,ctime,filesize,filetype,path,parentsfolder);
-		  
-			next();
+			//sql = 'update english set phonetic = ?,wordGroup = ?,example = ? where id = ?'
+			//sql = "insert into lesson_info values ('%s', '%s','%s','%s','%s','%s')" % (name, link, des, number, time, degree)
+			//// 将值封装为一个数组传值.
+			//db.run("UPDATE tbl SET name = ? WHERE id = ?", [ "bar", 2 ]);
 
-		}, function (err) {
-		  if (err) throw err;
-		  // 完成
-		  var endTimestamp = Date.parse(new Date())/1000;
-			var timeLong = (endTimestamp - startTimestamp);
-			console.log('总耗时' + timeLong + '秒');
-			stmt.finalize();
-			db.close();
+			// 异步遍历目录下的所有文件
+			rd.eachFileFilter('e:/',filePattern,function (f, s, next) {
+			  // 每找到一个文件都会调用一次此函数
+			  // 参数s是通过 fs.stat() 获取到的文件属性值
+		
+			  var filefullPath = f.replace(/\\/g,'/');
+			  var arr = filefullPath.split('/');
+			
+				var filename = arr[arr.length - 1];
+
+				var path = filefullPath.replace(filename,'');
+				var parentsfolder = arr[arr.length - 2];
+				var ctime = Date.parse(s.ctime)/1000
+				var mtime = Date.parse(s.mtime)/1000
+				var filesize = s.size;
+				var filetype = filename.substring(filename.lastIndexOf(".")+1);
+				console.log('filename',filename);
+				stmt.run(null,filename,mtime,ctime,filesize,filetype,path,parentsfolder);
+				next();
+
+			}, function (err) {
+			  if (err) throw err;
+			  // 完成
+			  console.log('startTimestamp',startTimestamp);
+			  var endTimestamp = Date.parse(new Date())/1000;
+				var timeLong = (endTimestamp - startTimestamp);
+				console.log('总耗时' + timeLong + '秒');
+				stmt.finalize();
+				db.close();
+			});
+		 
+		}, function() {
+			console.log('已取消');
 		});
+
+	/*
+
+
+		*/
 		
 	
 	
@@ -235,6 +274,17 @@
 	
 	
 	};
+	this.userDir = function(){	
+		//const shell = require('electron').shell
+		var {remote} = require('electron');
+		var {shell} = remote;
+		var os = require('os');
+		console.log('os.homedir',os.homedir());
+		//C:\\Users\\Administrator
+		//e:\\BaiduYunDownload\\DM王朝1566.2007.46集全
+		shell.showItemInFolder(os.homedir());
+	
+	},
 	this.thumTojpg = function(filepath,filename){
 		var ffmpeg = require('fluent-ffmpeg');
 		var fullpath = filepath + filename;
@@ -247,7 +297,7 @@
 		*/
 		ffmpeg(fullpath)
 		  .screenshots({
-			timestamps: ['20%','40%'],
+			timestamps: ['10%','20%','40%'],
 			filename: '%b-at-%s-seconds.png',
 			folder: './output'
 			//size: '320x240'
